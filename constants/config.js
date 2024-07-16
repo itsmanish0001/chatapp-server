ffe394d1388c89be5dd3128f33def2bc43e2dc1d
Manish Bharti <itsmanish@gmail.com>
2024-07-16 09:28:21 +0530

    undo

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: "*",
+    origin: ["http://localhost:5173", "http://localhost:4173" , process.env.CLIENT_URL],
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
